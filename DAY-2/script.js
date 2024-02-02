f95958921e8f33cd9e139b152590db4013af4703
Ndukaegho <sabastineaminaho3@gmail.com>
2024-02-03 00:28:20 +0100

    first commit

--- DAY-2/script.js
+++ DAY-2/script.js
@@ -12,7 +12,23 @@ let y;
 output = name;
 output = x;
 output = y;
-output = school;
-output = itMyDay;
+// output = school;
+// output = itMyDay;
 
-console.log(output)
+console.log(output);
+console.log(output, typeof output);
+
+
+const nameArr = ['Sabastine', 'Faith', 'Augustus']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const joinNums = numbers.concat(45, 47, 60);
+
+const schoolArr = school.split(' ').concat('Rivers', 'State', 'Nigeria');
+
+const chg = school.replace('Harcourt', 'Delta')
+// console.log(schoolArr[2] = 'Pit')
+console.log(schoolArr);
+console.log(joinNums);
+console.log(`${nameArr[0]} is the last born in the house of ${nameArr[1]} and ${nameArr[2]}`);
+console.log(chg)
